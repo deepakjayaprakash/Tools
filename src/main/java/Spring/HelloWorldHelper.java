834124c990bdfbcce68b24fa0c25b6e6359d94b8
deepakjayaprakash <deepakjpkodlekere@gmail.com>
2018-11-12 15:42:43 +0530

    added slf4j

--- src/main/java/Spring/HelloWorldHelper.java
+++ src/main/java/Spring/HelloWorldHelper.java
@@ -1,11 +1,19 @@
 package Spring;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Created by deepak.jayaprakash on 12/11/18.
  */
 public class HelloWorldHelper {
 
+    private static final Logger logger = LoggerFactory.getLogger(HelloWorldHelper.class);
+
     public void printStaticText() {
         System.out.println("Hello from HelloWorldHelper");
+        logger.info("info log {}:"+ 1.0);
+        logger.debug("debug log {}:"+ 2.0);
+        logger.error("error log {}:"+ 3.0);
     }
 }
